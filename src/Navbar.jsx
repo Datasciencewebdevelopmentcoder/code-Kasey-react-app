3855894533531fa102f50d28edf1192ccc0dfae5
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-22 02:56:30 +0000

    new

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = function () {
         <div className="links">
         <a href="/">Home</a>
         <a href="create" style={{
-            color: "blue",
+            color: "white",
             backgroundColor: "#f1356d",
             borderRadius: '8px'
         }}> New Blog</a>
