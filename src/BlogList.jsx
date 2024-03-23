70f83395397ae45e1503ae22863e8d6edcf9e030
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-23 19:39:56 +0000

    small updates

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -4,7 +4,6 @@ const BlogList = ({ blogs }) => {
         {blogs.map(blog => (
           <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
-            <p>{blog.body}</p>
             <p>Written by { blog.author }</p>
           </div>
         ))}
