98f726ae6bbcf54ce2bb4f5fe5ad0cba93593841
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-26 01:14:31 +0000

    lesson 23 finished

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,18 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const link = 'https://ubiquitous-rotary-phone-x554744v7w6g3xq6-8000.app.github.dev/blogs'
 const message = ' run [npx json-server data/db.json --port 8000 in new terminal] and runset port to public temporary'
-const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending , setIsPending] = useState(true)
-  const [error, setError] = useState(null)
 
-  useEffect(() => {
-    fetch(link)
-      .then(res => {
-        if(!res.ok == true) {
-          throw Error('could not fetch the data')
-        }
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false)
-        setError(null)
-      })
-      .catch(err => {
-        setIsPending(false)
-        setError(err.message)
-      })
-  }, [])
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch(link)
 
   return (
     <div className="home">
-      {error && <div>{error}</div>}
-      {isPending && <div>Loading ....</div>}
-      {blogs && <BlogList blogs={blogs} />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
- 
-export default Home;
+export default Home
